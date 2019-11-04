9795fe18572b685b04db25c55c0f02d633ac8809
Chidimma <chidimmai@gmail.com>
2019-11-04 09:00:14 -0600

    Updating Files

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,72 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  title = "Grocery";
 
-  constructor(public navCtrl: NavController) {
+  items = [
+    {
+      name: "Eggs",
+      quantity: 1
+    },
+    {
+      name: "Bread",
+      quantity: 2
+    },
+    {
+      name: "Green Pepper",
+      quantity: 3 
+    },
+    {
+      name: "Carrot",
+      quantity: 1
+    },
+    {
+      name: "Apple",
+      quantity: 9
+    },
+    {
+      name: "Butter",
+      quantity: 2
+    },
+    {
+      name: "Onion",
+      quantity: 5
+    },
+    {
+      name: "Avocado",
+      quantity: 4
+    },
+    {
+      name: "Oatmeal",
+      quantity: 2
+    },
+    {
+      name: "Coconut",
+      quantity: 3 
+    },
+    {
+      name: "Rice",
+      quantity: 1 
+    },
+  ];
 
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+
+  }
+
+  removeItem(item) {
+    console.log("Removing Item -", item);
+    const toast = this.toastCtrl.create({
+      message: 'Removing Item - ' + item.name + " ...",
+      duration: 3000
+    });
+    toast.present();
   }
 
 }
